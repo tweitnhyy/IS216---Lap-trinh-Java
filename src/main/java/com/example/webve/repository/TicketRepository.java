d62407d045d27f699d14c3376b56ceaaf55ba308
Tuyet Nhi <tweitnhyy@gmail.com>
2025-04-29 20:52:56 +0700

    Xoá toàn bộ file khỏi Git nhưng giữ trên máy

--- src/main/java/com/example/webve/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.webve.repository;
-
-import com.example.webve.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
