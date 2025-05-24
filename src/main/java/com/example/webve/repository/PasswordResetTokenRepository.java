c3f66bbb16ec7276cfb132387ee606b62eb5ec3d
tweitnhyy <23521105@gm.uit.edu.vn>
2025-05-24 13:04:41 +0700

    Delete src/main/java/com/example/webve/repository directory

--- src/main/java/com/example/webve/repository/PasswordResetTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webve.repository;
-
-import com.example.webve.model.PasswordResetToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-    PasswordResetToken findByToken(String token);
-}
