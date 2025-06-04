796c4f1b4c9d41916c47626e8be28c2aa795928b
Thái Bình Dương <23520356@gm.uit.edu.vn>
2025-06-04 15:40:18 +0700

    Update OrderService.java

--- src/main/java/com/example/vnpay/service/OrderService.java
+++ src/main/java/com/example/vnpay/service/OrderService.java
@@ -11,12 +11,6 @@ public class OrderService {
 
     public synchronized String createOrder(OrderDTO request) {
         String orderId = UUID.randomUUID().toString();
-        double totalAmount = request.getPrice() * request.getQuantity();
-
-        // Save to DB using JPA or JDBC (pseudo code):
-        // orderRepository.save(new Order(...));
-
-        System.out.println("Created order: " + orderId + " with amount: " + totalAmount);
         return orderId;
     }
 }
